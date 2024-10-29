6c4c545b53af0252ca20d0a552ed8338403ba967
Eddy Adegnandjou <adegnandjoueddy12@gmail.com>
2024-10-29 05:49:25 +0100

    test: add user account model creation tests

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
+
+class UserAccountModelTest(TestCase):
+    def test_create_user(self):
+        User = get_user_model()
+        user = User.objects.create_user(username="eddy", password="password")
+        self.assertEqual(user.username, "eddy")
+
+    def test_create_superuser(self):
+        User = get_user_model()
+        admin_user = User.objects.create_superuser(
+            username="admin", password="password"
+        )
+        self.assertTrue(admin_user.is_superuser)
