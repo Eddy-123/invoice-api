668b5eb9cf12e635aa83902b68ec9da4c7e037c3
Eddy Adegnandjou <adegnandjoueddy12@gmail.com>
2024-11-04 05:44:51 +0100

    feat: generate invoice pdf

--- invoices/urls.py
+++ invoices/urls.py
@@ -1,9 +1,19 @@
 from django.urls import path
-from .views import FileUploadAPIView, InvoiceDetailAPIView, InvoiceListAPIView
+from .views import (
+    FileUploadAPIView,
+    InvoiceDetailAPIView,
+    InvoiceListAPIView,
+    FileDownloadAPIView,
+)
 
 
 urlpatterns = [
     path("upload/", FileUploadAPIView.as_view(), name="upload-invoice-file"),
     path("invoices/", InvoiceListAPIView.as_view(), name="invoice-list"),
     path("invoice/<int:pk>/", InvoiceDetailAPIView.as_view(), name="invoice-detail"),
+    path(
+        "download/invoice/<int:pk>/",
+        FileDownloadAPIView.as_view(),
+        name="invoice-download",
+    ),
 ]
